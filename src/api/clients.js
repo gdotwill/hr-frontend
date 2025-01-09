75ffac9155ada6f22476edecd114fc84123cc47f
gdotwill <godwillmark@yahoo.co.uk>
2025-01-09 14:06:25 +0200

    update url

--- src/api/clients.js
+++ src/api/clients.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV === 'production' ? 'https://hr-c.vercel.app/api/clients' : 'http://lhost:3000/api/clients';
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://hr-backend-eight.vercel.app/api/clients' : 'http://lhost:3000/api/clients';
 
 export default axios.create({
     baseURL,
